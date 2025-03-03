fa5ecc30c5fec7e86f8ec7d7d818fffb5fc3474c
prashantbhadoria1902 <prashantbhadoria1902@gmail.com>
2025-03-03 11:14:01 +0530

    Added

---
+++ src/test/java/com/example/restapicall/RestapicallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapicall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapicallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
