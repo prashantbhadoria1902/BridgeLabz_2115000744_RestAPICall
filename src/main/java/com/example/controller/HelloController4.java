a6c4727c1492a9b1380f8b502c8a3efe1d23b492
prashantbhadoria1902 <prashantbhadoria1902@gmail.com>
2025-03-03 11:59:50 +0530

    [Prashant] Added UC4 assignment

---
+++ src/main/java/com/example/controller/HelloController4.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dto.UserDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController4 {
+
+    @PostMapping("/post")
+    public String sayHello(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+}
